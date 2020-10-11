b3a1ff34d605c9622bfa2bfd5ad0ac70cde5e843
onias17 <oniasnephiisrael@gmail.com>
2020-10-11 16:10:30 -0700

    song new route created

--- controllers/songsControllers.js
+++ controllers/songsControllers.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const db = require('../models');
 
+// GET INDEX
+// router.get('/', (req, res) => {
+//     res.render()
+// });
 
+// GET NEW
+router.get('/new', (req, res) => {
+    res.render('songs/new')
+});
 
 module.exports = router;
