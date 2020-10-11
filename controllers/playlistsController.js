e82056365209b345c90ba678f2ae6c8b74f9176c
Howakyl96 <howakyl@gmail.com>
2020-10-11 13:05:06 -0700

    commit 8 - added playlist index route and view

--- controllers/playlistsController.js
+++ controllers/playlistsController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Playlist } = require('../models');
 const router = express.Router();
 
 const db = require('../models');
@@ -7,13 +8,31 @@ const db = require('../models');
 //GET INDEX
 router.get('/' , (req,res) => {
 
-    res.render('playlists/index');
+    db.Playlist.find({}, (err,allPlaylists) => {
+        if (err) return console.log(err);
+        const context = {
+            playlists: allPlaylists
+        };
+    
+        res.render('playlists/index' , context);
+    });
 });
 
 //GET NEW
 router.get('/new' , (req,res) => {
     res.render('playlists/new');
 });
+
+//POST - Create playlist
+router.post('/' , (req,res) => {
+    db.Playlist.create(req.body, (err, newPlaylist) => {
+        if (err) return console.log(err);
+
+        res.redirect('/playlists');
+    });
+});
+
+
 
 
 
