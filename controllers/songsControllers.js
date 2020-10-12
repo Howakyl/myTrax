b79ae95b864b61d955f1166dece8a382f6c604ae
onias17 <oniasnephiisrael@gmail.com>
2020-10-11 17:18:32 -0700

    commit 20 - created song index route

--- controllers/songsControllers.js
+++ controllers/songsControllers.js
@@ -4,13 +4,45 @@ const router = express.Router();
 const db = require('../models');
 
 // GET INDEX
-// router.get('/', (req, res) => {
-//     res.render()
-// });
+router.get('/', (req, res) => {
+    db.Song.find({}, (err, allSongs) => {
+        if(err) return console.log(err);
+
+        const context = {
+            songs: allSongs,
+        };
+        res.render('songs/index', context);
+    });
+});
 
 // GET NEW
 router.get('/new', (req, res) => {
-    res.render('songs/new')
+    db.Playlist.find({}, (err, allPlaylists) => {
+        if(err) return console.log(err);
+
+        const context = {
+            playlists: allPlaylists
+        };
+        res.render('songs/new', context);
+    });
+});
+
+// POST - create song
+router.post('/', (req, res) => {
+    db.Song.create(req.body, (err, newSong) => {
+        if(err) return console.log(err);
+
+        db.Playlist.findById(req.body.playlist, (err, foundPlaylist) => {
+            if(err) return console.log(err);
+            foundPlaylist.songs.push(newSong._id);
+
+            foundPlaylist.save((err, savedPlaylist) => {
+                if(err) return console.log(err);
+                console.log(savedPlaylist);
+                res.send('song added');
+            });
+        });
+    });
 });
 
 module.exports = router;
